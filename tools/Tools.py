ad69ff245bd37c35b7846130c7c182a08b5a6c50
NagiPragalathan <kk2069037@gmail.com>
2023-01-05 09:49:32 +0530

    Tools.py are updated

--- tools/Tools.py
+++ tools/Tools.py
@@ -1,4 +1,18 @@
+import os
+import pathlib
+
+
 def word_to_var(word):
     return "_".join(word.split())
 
-print(word_to_var("String Split and Join"))
+def create_files():
+    files = ['solution.py','question.txt','question.md']
+    path = os.path.abspath(__file__)
+    a=pathlib.Path(path).as_uri().split('tools')
+    new_folder = a[0]+word_to_var(input("Enter string : ")+'/')
+    os.mkdir(new_folder[8:])
+    for i in files:
+        with open(new_folder[8:]+i,'w') as file:
+            file.close()
+
+create_files()
