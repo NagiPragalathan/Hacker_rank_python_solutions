99e33164978a89e0393cd574b1cd8b4054a2a334
NagiPragalathan <kk2069037@gmail.com>
2023-01-05 07:46:10 +0530

    crt ans updated in odd or even

--- odd_or_even/odd_or_even.py
+++ odd_or_even/odd_or_even.py
@@ -9,7 +9,12 @@ import sys
 
 def check_odd_even(number):
     if(number%2 == 0):
-        return "Not Weird"
+        if (number>=2 and 5>=number):
+            return "Not Weird"
+        elif (number>=6 and 20>=number):
+            return "Weird"
+        elif(20<number):
+            return 'Not Weird'
     else:
         return "Weird"
 
